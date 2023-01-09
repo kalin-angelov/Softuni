6081665ebf520b74189716b47d0284d15e18ae92
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-01-09 14:37:29 +0200

    Create 07. Required reading.js

---
+++ JS Fundamentals/03.Data-Types-and-Variables-Lab/07. Required reading.js
@@ -0,0 +1,7 @@
+function requiredReading(bookPages, pagesForHour, givenTimeInDays) {
+
+    let result = (bookPages / pagesForHour) / givenTimeInDays;
+    console.log(result);
+
+}
+requiredReading(212, 20, 2)
