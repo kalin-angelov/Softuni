08e904c96618163bfc0905035420171e7e0505f1
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-01-10 21:04:33 +0200

    Create 03.Town Info.js

---
+++ JS Fundamentals/04.Data Types and Variables - Exercise/03.Town Info.js
@@ -0,0 +1,5 @@
+function townInfo(townName, population, areaNumber) {
+
+    console.log(`Town ${townName} has population of ${population} and area ${areaNumber} square km.`);
+}
+townInfo('Sofia',1286383,492)
