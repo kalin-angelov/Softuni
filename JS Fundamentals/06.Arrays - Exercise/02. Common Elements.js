f6f8d9e655c9cc04352340ca5fe54a0307377347
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-01-10 21:21:17 +0200

    Create 02. Common Elements.js

---
+++ JS Fundamentals/06.Arrays - Exercise/02. Common Elements.js
@@ -0,0 +1,14 @@
+function commonElements(array1, array2) {
+
+    for (let el1 of array1) {
+        
+        for (let el2 of array2) {
+
+            if (el1 === el2) {
+                console.log(el1);
+            }
+        }
+    }
+
+}
+commonElements(['S', 'o', 'f', 't', 'U', 'n', 'i', ' '],['s', 'o', 'c', 'i', 'a', 'l'])
