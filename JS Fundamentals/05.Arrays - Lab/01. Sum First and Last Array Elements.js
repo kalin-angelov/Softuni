364318d00d6e05ad461bc0b25cb43253a1ff050c
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-01-10 21:09:19 +0200

    Create 01. Sum First and Last Array Elements.js

---
+++ JS Fundamentals/05.Arrays - Lab/01. Sum First and Last Array Elements.js
@@ -0,0 +1,8 @@
+function sumFirstAndLastArrayElements(numbers) {
+
+    let firstNumber = numbers[0];
+    let lastNumber = numbers[numbers.length - 1];
+
+    console.log(firstNumber + lastNumber);
+}
+sumFirstAndLastArrayElements([20, 30, 40])
