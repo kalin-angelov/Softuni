7f4b3b966dc7720238c703bd07dd5cc5f3cf8aa0
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-01-14 22:12:19 +0200

    Create 01. Smallest of Three Numbers.js

---
+++ JS Fundamentals/08.Functions - Exercise/01. Smallest of Three Numbers.js
@@ -0,0 +1,6 @@
+function smallestOfThreeNumbers(firstNumber, secondNumber, thirdNumber) {
+
+    let minNumber = Math.min(firstNumber, secondNumber, thirdNumber);
+    console.log(minNumber);
+}
+smallestOfThreeNumbers(600, 342, 123)
