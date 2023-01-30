48d4e69821f2fbf2f57f4ea35a4a5a5fcc13820c
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-01-30 21:55:25 +0200

    Create 01. City Record.js

---
+++ JS Advanced/JS Advanced/05.Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,13 @@
+function cityRecord(name, population, treasury) {
+
+    let townInformation = {
+        name,
+        population,
+        treasury,
+    };
+
+    return townInformation;
+    
+}
+console.log(cityRecord('Tortuga',7000,15000));
+console.log(cityRecord('Santo Domingo',12000,23500));
