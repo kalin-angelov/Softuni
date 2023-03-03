f8babc997cca5b3d09baebf9feb4795a536b75ea
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-03-03 19:54:53 +0200

    Create 05.commandProcessor.js

---
+++ JS Advanced/JS Advanced/11.Advanced Functions - Lab/05.commandProcessor.js
@@ -0,0 +1,19 @@
+function solution() {
+
+    let str = '';
+    return {
+        append: (s) => str += s,
+        removeStart: (n) => str = str.substring(n),
+        removeEnd: (n) => str = str.substring(0, str.length - n),
+        print: () => console.log(str)  
+    }
+}
+
+
+let secondZeroTest = solution();
+
+secondZeroTest.append('123');
+secondZeroTest.append('45');
+secondZeroTest.removeStart(2);
+secondZeroTest.removeEnd(1);
+secondZeroTest.print();
